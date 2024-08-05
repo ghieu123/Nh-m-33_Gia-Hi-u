66f57b3e521fdc218e8fef62592b155f8f968574
ghieu123 <giahieuc5@gmail.com>
2024-08-05 17:50:00 +0700

    Cau 3,1_Buoi 2_Bai 2

---
+++ Buổi 2/Bài 2 Các giải thuật tìm kiếm và sắp xếp/Câu 3/Cau 3,1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+
+// Hàm kiểm tra số nguyên tố
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 == 0 || num % 3 == 0) return false;
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+// Hàm liệt kê số nguyên tố nhỏ hơn n
+void listPrimesLessThanN(int n) {
+    vector<int> primes;
+    for (int i = 2; i < n; ++i) {
+        if (isPrime(i)) primes.push_back(i);
+    }
+    if (primes.empty()) {
+        cout << "Khong co so nguyen to nao nho hon  " << n << endl;
+    }
+    else {
+        cout << "So nguyen to nho hon n " << n << ": ";
+        for (int prime : primes) {
+            cout << prime << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<int> arr;
+    int n;
+
+    // Nhập số nguyên n
+    cout << "Nhap so n: ";
+    cin >> n;
+
+    // Nhập mảng
+    cout << "Nhap so phan tu trong mang: ";
+    int size;
+    cin >> size;
+    arr.resize(size);
+    cout << "Nhap cac phan tu cua mang: ";
+    for (int& num : arr) {
+        cin >> num;
+    }
+
+    // 1. Liệt kê số nguyên tố nhỏ hơn n
+    listPrimesLessThanN(n);
+
+
+    _getch();
+    return 0;
+};
