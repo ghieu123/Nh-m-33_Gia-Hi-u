cd9d886ac7ac656d484f994ee05c1b3487ee2191
ghieu123 <giahieuc5@gmail.com>
2024-08-05 17:23:29 +0700

    Cau 13_Bai 1_Buoi 1

---
+++ Buổi 1/Bài 1 Kỹ thuật xử lý mảng 1 chiều, con trỏ, xử lý ngoại lệ/Cau 13.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <stdexcept>  // Thư viện cho ngoại lệ chuẩn
+
+using namespace std;
+
+void divideArrays(const vector<int>& a, const vector<int>& b) {
+    if (a.size() != b.size()) {
+        throw invalid_argument("Kich thuoc cua 2 mang khong bang nhau.");
+    }
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (b[i] == 0) {
+            cout << "Phep chia " << a[i] << " / " << b[i] << " khong hop le (chia cho 0)" << endl;
+        }
+        else {
+            cout << "Ket qua cua Phep chia" << a[i] << " / " << b[i] << " = " << static_cast<double>(a[i]) / b[i] << endl;
+        }
+    }
+}
+
+int main() {
+    vector<int> a, b;
+    int n, temp;
+
+    cout << "Nhap so luong cua phan tu mang a va b: ";
+    cin >> n;
+
+    cout << "Nhap cac phan tu cua mang a: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> temp;
+        a.push_back(temp);
+    }
+
+    cout << "Nhap cac phan tu cua mang b: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> temp;
+        b.push_back(temp);
+    }
+
+    try {
+        divideArrays(a, b);
+    }
+    catch (const invalid_argument& e) {
+        cout << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
